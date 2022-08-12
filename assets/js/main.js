d5382f4aae92967280d8cf20d639217a4f09249f
KatKlee <katklee2022@gmail.com>
2022-08-12 13:46:57 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+let languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby",
+    "PHP",
+    "C++",
+    "CSS",
+    "C#",
+    "Go",
+    "C",
+    "TypeScript",
+    "Swift"
+]
+
+
+/* Sortierung der Programmiersprachen */
+const sortierung2 = () => {
+    console.log(languages.sort())
+}
+sortierung2()
+
+
+
+/* Umgekehrte Sortierung der Programmiersprachen */
+const sortierung3 = () => {
+    console.log(languages.reverse())
+}
+sortierung3()
+
